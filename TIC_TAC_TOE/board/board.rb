6b91e703192a03cfbc66034182d7c702c68d5486
krzysztof.tokarski <krzysztof.tokarski96@gmail.com>
2022-07-09 21:57:00 +0200

    refactor: fixes to board class methods

--- TIC_TAC_TOE/board/board.rb
+++ TIC_TAC_TOE/board/board.rb
@@ -7,35 +7,41 @@ class Board
   end
 
   def set_cell_state(marker,y_coordinate,x_coordinate)
-    puts self.cells_state
+    p self.cells_state
     self.cells_state[y_coordinate][x_coordinate] = marker;
-    puts self.cells_state
+    p self.cells_state
   end
 
   # def check_if_win()
   # end
 
   def check_if_cells_arrays_have_only_repeats(array)
-    puts self.cells_state
-    array.cells_state.each do |row|
-      if row.exclude? nil && row.uniq.size === 1
+    p self.cells_state
+    array.each do |row|
+      unless row.include? nil && row.uniq.size != 1
         puts "#{row.first} wins the game!"
+      else
+        puts "nope"
       end
     end
   end
 
   def represent_columns_like_rows()
-    puts self.cells_state
+    p self.cells_state
     columns_represented_like_rows = self.cells_state.map.with_index do |row, iteration_index|
+      p row
+      p iteration_index
       row = []
       3.times do |index|
+        p index
         row.push(self.cells_state[iteration_index][index])
       end
     end
+    p columns_represented_like_rows
   end
 
   def represent_diagonals_like_rows()
-    puts self.cells_state
+    p self.cells_state
     arrays_array = Array.new(2) {Array.new(3, nil)}
 
     3.times do |index|
