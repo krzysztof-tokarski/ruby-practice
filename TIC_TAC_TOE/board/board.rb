333d77f56fbf5413314f9d968b8230f8702d8dac
krzysztof.tokarski <krzysztof.tokarski96@gmail.com>
2022-07-09 22:09:15 +0200

    feat: add check_if_win func and fixes to repeats

--- TIC_TAC_TOE/board/board.rb
+++ TIC_TAC_TOE/board/board.rb
@@ -7,41 +7,40 @@ class Board
   end
 
   def set_cell_state(marker,y_coordinate,x_coordinate)
-    p self.cells_state
     self.cells_state[y_coordinate][x_coordinate] = marker;
-    p self.cells_state
   end
 
-  # def check_if_win()
-  # end
+  def check_if_win
+    horizontal_win_conditions = self.cells_state
+    vertical_win_conditions = self.represent_columns_like_rows
+    diagonal_win_conditions = self.represent_diagonals_like_rows
+    win_conditions = [horizontal_win_conditions,vertical_win_conditions,diagonal_win_conditions]
+
+    win_conditions.each do |array|
+      self.check_if_cells_arrays_have_only_repeats(array)
+    end
+  end
 
   def check_if_cells_arrays_have_only_repeats(array)
-    p self.cells_state
     array.each do |row|
-      unless row.include? nil && row.uniq.size != 1
+      if !(row.include? nil) && row.uniq.size == 1
         puts "#{row.first} wins the game!"
-      else
-        puts "nope"
       end
     end
   end
 
   def represent_columns_like_rows()
-    p self.cells_state
-    columns_represented_like_rows = self.cells_state.map.with_index do |row, iteration_index|
-      p row
-      p iteration_index
+    columns_represented_like_rows = self.cells_state.map.with_index do |row, static_index|
       row = []
       3.times do |index|
-        p index
-        row.push(self.cells_state[iteration_index][index])
+        row.push(self.cells_state[index][static_index])
       end
+      row
     end
-    p columns_represented_like_rows
+    columns_represented_like_rows
   end
 
   def represent_diagonals_like_rows()
-    p self.cells_state
     arrays_array = Array.new(2) {Array.new(3, nil)}
 
     3.times do |index|
@@ -54,3 +53,4 @@ class Board
     arrays_array
   end
 end
+
